b070f5119968bf63dc6fb27884e7cdc05cdbaf92
Gabriel Rolón <gabrielomarrolon1996@gmail.com>
2025-11-05 21:33:28 -0300

    Componentes base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/auth/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+    children: [/* Aca van las rutas que van a tener que poner la ruta a su componente para que la ruta sea menu/ejemplo
+      {
+        path: 'ejemplo',
+        loadComponent: () => import('./components/ejemplo/ejemplo.component').then(m => m.EjemploComponent)
+      },
+*/
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  imports: [RouterOutlet],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  
+  isMenuOpen = true;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
